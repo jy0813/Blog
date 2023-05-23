4b59ef92fe00be1092ea8469a0c2b4b742029218
jy0813 <busbug3@gmail.com>
2023-05-24 00:01:37 +0900

    feat: useInterval hooks 추가

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,18 @@
+import { useEffect, useLayoutEffect, useRef } from 'react';
+
+const UseInterval = (callback: () => void, delay: number | null) => {
+  const savedCallback = useRef(callback);
+  useLayoutEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (!delay && delay !== 0) return;
+
+    const id = setInterval(() => savedCallback.current(), delay);
+
+    return () => clearInterval(id);
+  }, [delay]);
+};
+
+export default UseInterval;
